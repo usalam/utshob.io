210dcf160e14167486444e9c7507e2dd7ca9478c
utshob-alam <143423763+utshob-alam@users.noreply.github.com>
2025-07-19 21:46:42 -0400

    added mobile hamburger nav

--- script.js
+++ script.js
@@ -136,3 +136,12 @@ const fadeObserver = new IntersectionObserver(
 );
 
 animatedSections.forEach(section => fadeObserver.observe(section));
+
+
+// Mobile menu toggle
+const toggleBtn = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+});
